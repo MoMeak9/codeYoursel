3ff23dc06d6b87a5388138ac0eaeacb0b2faabe7
MoMeak9 <shi.yihui@iwhalecloud.com>
2022-10-06 21:58:56 +0800

    feat: 场景方法手撕

---
+++ Environment/imgLoad.js
@@ -0,0 +1,20 @@
+const imageAsync=(url)=>{
+    return new Promise((resolve,reject)=>{
+        let img = new Image();
+        img.src = url;
+        img.οnlοad=()=>{
+            console.log(`图片请求成功，此处进行通用操作`);
+            resolve(img);
+        }
+        img.οnerrοr=(err)=>{
+            console.log(`失败，此处进行失败的通用操作`);
+            reject(err);
+        }
+    })
+}
+
+imageAsync("url").then(()=>{
+    console.log("加载成功");
+}).catch((error)=>{
+    console.log(error);
+})
